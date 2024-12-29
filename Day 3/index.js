65b135089443dba2b55d53dafcd086c56d59b8ad
João Pedro Barboza <barbozajoaop@gmail.com>
2024-12-29 20:16:05 -0300

    On day 3 we learn about the difference of 'let and const' & user inputs commands.

---
+++ Day 3/index.js
@@ -0,0 +1,7 @@
+//let and const
+let name1 = 'João Pedro Barboza';
+console.log (name1)
+
+//input
+let userInput = prompt('What year are you born?');
+console.log ('You have ' + (2024 - userInput) + ' years old')
